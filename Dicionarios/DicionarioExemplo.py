69200f25949b1da30e47b39182df9e2074b11022
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-13 18:20:47 -0300

    first commit

--- Dicionarios/DicionarioExemplo.py
+++ Dicionarios/DicionarioExemplo.py
@@ -1,2 +1,10 @@
 usuarios = {}
 print(usuarios)
+
+usuarios = {
+    "chaves" : ["Chaves do 8", "24/12/2017", "Recep_01"],
+    "quico" : ["Quico das flores", "24/12/2018", "Recep_02"]
+
+}
+
+print(usuarios)
