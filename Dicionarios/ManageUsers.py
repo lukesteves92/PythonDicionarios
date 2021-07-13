51eae6bb1538807bd9d06e1c0afcae8e4085fa9d
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-13 18:50:44 -0300

    MenuDicionario

--- Dicionarios/ManageUsers.py
+++ Dicionarios/ManageUsers.py
@@ -5,9 +5,5 @@ opcao = perguntar()
 
 while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
     if opcao=="I":
-        chave=input("Digite o login:").upper()
-        nome = input("Digite o nome:").upper()
-        data = input("Digite a última data de acesso:").upper()
-        estacao = input("Qual a última estação acessada").upper()
-        usuarios[chave] = [nome, data, estacao]
+        usuarios[input("Digite o login:").upper()] = [input("Digite o nome:").upper(), input("Digite a última data de acesso:").upper(), input("Qual a última estação acessada").upper()]
     opcao = perguntar()
